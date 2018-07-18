adf964894457f3229d018cb2ea5a66af57ee8f59
Nicholas Kennedy <nick.kennedy@rangle.io>
2018-07-18 04:26:23 -0300

    Fix test

--- src/routes/health/health.test.js
+++ src/routes/health/health.test.js
@@ -1,7 +1,6 @@
 const request = require("supertest");
 
 const { healthRouter } = require("./health.router");
-const { app } = require("../../index");
 
 describe("tests for the health check endpoint", () => {
   it("returns a 200", () => {
