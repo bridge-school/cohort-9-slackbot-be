2a98aa129d0eae546ff95c6da2663380f4ff20ca
Chaoyue <chaoyue.zhao.2011@gmail.com>
2019-12-10 23:22:24 -0500

    added updateDatabase function

--- src/routes/responses/responses.controller.js
+++ src/routes/responses/responses.controller.js
@@ -1,18 +1,53 @@
 const _get = require('lodash/get');
+const db = require("./../../db/index");
 
+// Organize Response Object from Slack API
 const postResponses = function (req, res) {
   const body = JSON.parse(req.body.payload);
   const response = {
     responseSelected: _get(body, 'actions[0].text.text', ''),
-    questionID: _get(body, 'message.blocks[0].block_id', '')
+    id: _get(body, 'actions[0].block_id', '')
   };
-  console.log(response);
-  res.set('Content-Type', 'application/json')
-  res.send(`Received new Poll: ${body.message}`);
+
+  res.set('Content-Type', 'application/json');
+  res.send(`Received new Poll: ${response}`);
+
+  updateDatabase(response);
 }
+
+const getData = response => {
+  return (
+    db
+      .collection("SLACKBOT_TEST")
+      .doc(response.id)
+      .get()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('No such document!');
+        } else {
+          return doc.data();
+        }
+      })
+  );
+};
+
+const updateDatabase = async (response) => {
+  await getData(response)
+    .then(data => {
+      const initialResponse = data.responses[response.responseSelected];
+
+      db.collection("SLACKBOT_TEST").doc(response.id).update({
+        responses: {
+          ...data.responses,
+          [`${[response.responseSelected]}`]: initialResponse + 1
+        }
+      })
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Something broke!");
+    });
+};
 
 module.exports = { postResponses };
 
-// check to see how do we want to filter the poll question
-
-// update firebase with information 
